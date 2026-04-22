1656adbc67537d733c5a930858b9450d1ed1c477
Xumoynxon <158552920+sxumoyun@users.noreply.github.com>
2026-04-22 17:43:59 +0500

    issue fixed

--- nafas-backend/src/main.ts
+++ nafas-backend/src/main.ts
@@ -6,13 +6,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://nafas-1.onrender.com', // Frontend manzilingiz
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
-  console.log('Server ishga tushdi: http://localhost:3000');
+  console.log('Server ishga tushdi: https://nafas-khle.onrender.com');
 }
 bootstrap();
